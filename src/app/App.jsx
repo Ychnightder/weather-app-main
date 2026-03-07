5ef9d58c4f0469a15d693da96a5b81fb3601c18f
Ychnightder <pierreychnightder561@gmail.com>
2026-03-07 13:48:03 +0100

    fix

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,9 @@
 import { Nav } from '../components/nav/nav';
 import { Title } from '../components/title/Title';
-import { ButtonPrimary } from '../components/button/buttonPrimary';
-import { DropdownUnits } from './../components/dropdown/DropdownUnits';
-import { DropdownSearch } from '../components/dropdown/DropdownSearch';
-import { DropdownDays } from '../components/dropdown/DropdownDays';
+// import { ButtonPrimary } from '../components/button/buttonPrimary';
+// import { DropdownUnits } from './../components/dropdown/DropdownUnits';
+// import { DropdownSearch } from '../components/dropdown/DropdownSearch';
+// import { DropdownDays } from '../components/dropdown/DropdownDays';
 function App() {
 	return (
 		<div className="lg:px-28 lg:pt-12 md:px-20 md:pt-15 px-5 pt-10">
