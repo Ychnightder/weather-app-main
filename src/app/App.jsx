03b5c61ebde2c994e2228e86423eefe0249c37c9
Ychnightder <pierreychnightder561@gmail.com>
2026-03-09 18:01:16 +0100

    fix vercel

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,15 @@
-import { Title } from '../components/title/Title';
-import { Search } from '../components/search/Search';
-import { Nav } from '../components/nav/Nav';
+// import { Title } from '../components/title/Title';
+// import { Search } from '../components/search/Search';
+// import { Nav } from '../components/nav/Nav';
 
 function App() {
 	return (
 		<div className="lg:px-28 lg:pt-12 md:px-20 md:pt-15 px-5 pt-10">
-			<Nav />
+			{/* <Nav />
 			<Title text="How's the sky looking today?" />
 			<div className="w-full flex items-center justify-center">
 				<Search />
-			</div>
+			</div> */}
 		</div>
 	);
 }
