921efca1e4473c46db90abe1a1737b15acc84899
Ychnightder <pierreychnightder561@gmail.com>
2026-03-09 17:58:37 +0100

    Fixing exact file casing for Vercel

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,25 +1,17 @@
-import { Nav } from '../components/nav/Nav.jsx';
 import { Title } from '../components/title/Title';
- 
- // import { DropdownUnits } from './../components/dropdown/DropdownUnits';
- import { DropdownSearch } from '../components/dropdown/DropdownSearch';
- //  import { DropdownDays } from '../components/dropdown/DropdownDays';
- import { Search } from '../components/search/Search';
- function App() {
-		return (
-			<div className="lg:px-28 lg:pt-12 md:px-20 md:pt-15 px-5 pt-10">
-				<Nav />
-				<Title text="How's the sky looking today?" />
-				{/* <ButtonPrimary /> */}
-				{/* <DropdownUnits /> */}
-				{/* <DropdownSearch cities={null} /> */}
-				{/* <DropdownSearch cities={['paris' , 'london', 'new york', 'tokyo', 'sydney']} /> */}
-				{/* <DropdownDays /> */}
-				<div className="w-full flex items-center justify-center">
-					<Search />
-				</div>
+import { Search } from '../components/search/Search';
+import { Nav } from '../components/nav/Nav';
+
+function App() {
+	return (
+		<div className="lg:px-28 lg:pt-12 md:px-20 md:pt-15 px-5 pt-10">
+			<Nav />
+			<Title text="How's the sky looking today?" />
+			<div className="w-full flex items-center justify-center">
+				<Search />
 			</div>
-		);
- }
+		</div>
+	);
+}
 
 export default App;
