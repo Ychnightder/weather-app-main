89ae611a2520d8b9dc2c62a60aec96321976b117
Ychnightder <pierreychnightder561@gmail.com>
2026-03-09 18:22:12 +0100

    vercel

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,9 +7,9 @@ function App() {
 		<div className="lg:px-28 lg:pt-12 md:px-20 md:pt-15 px-5 pt-10">
 			<Nav />
 			<Title text="How's the sky looking today?" />
-			{/* <div className="w-full flex items-center justify-center">
-				<Search />
-			</div> */}
+			<div className="w-full flex items-center justify-center">
+				{/* <Search /> */}
+			</div>
 		</div>
 	);
 }
